180d8ddb27322b633edc9b345a013866e63f7ca6
Nikita <dimavlaskin9@gmail.com>
2023-11-25 00:40:51 +0300

    дополнена логика работы с базой данных

--- events/system/ready.js
+++ events/system/ready.js
@@ -1,9 +1,14 @@
 const { Events } = require('discord.js');
+const sequelize = require("../../db")
+const fs = require("fs")
+const path = require('path')
+
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
+    require('../../handlers/dbHandler').init()
     require('../../handlers/commandHandler').init(client)
     require('../../handlers/commandRegister').init(client)
   }
