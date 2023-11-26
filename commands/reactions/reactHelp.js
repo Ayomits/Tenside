f20047606616d1e38039b9072a18e2b585c3615e
PVA12 <106029090+PVA12@users.noreply.github.com>
2023-11-26 11:10:06 +0200

    тип реакции обновил

--- commands/reactions/reactHelp.js
+++ commands/reactions/reactHelp.js
@@ -1,8 +1,4 @@
-const {
-  CommandInteraction,
-  EmbedBuilder,
-  PermissionFlagsBits,
-} = require("discord.js");
+const { CommandInteraction, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const fs = require("fs");
 const path = require("path");
@@ -13,25 +9,23 @@ module.exports = {
     .setDescription("Все команды реакций")
     .setDMPermission(true),
 
-  /**
-   *
-   * @param {CommandInteraction} interaction
-   *
-   */
   async execute(interaction) {
-    const congfigs = path.resolve("configs");
-    const data = JSON.parse(
-      await fs.promises.readFile(congfigs + "/reactions.json")
-    );
-    const embed = new EmbedBuilder().setTitle("Помощь по командам реакций");
+    const configs = path.resolve("configs");
+    const data = JSON.parse(await fs.promises.readFile(configs + "/reactions.json"));
+    const doubleembed = new EmbedBuilder().setTitle("Помощь по командам реакций");
+    const embeds = [];
 
-    let description = "";
     for (let react in data) {
-      description += `**${data[react].api_name}** - ${data[react].action}\n`;
-    }
-
-    embed.setDescription(description);
-
-    await interaction.reply({ embeds: [embed] });
+      if (data[react].type === 'Любовные' || data[react].type === 'Эмоции' || data[react].type === 'Действия') {
+        const embed = new EmbedBuilder().setTitle(`${data[react].type} Команды`);
+        embed.setDescription(data[react].api_name + "-" + data[react].action);
+        embeds.push(embed);
+      }
+    else {
+      const embed = new EmbedBuilder().setTitle("Помощь по командам реакций");
+      embed.setDescription("Команд с такими категориями не существует.");
+      await interaction.reply({ embeds: [embed, embeds] });
+    }}
+    await interaction.reply({ embeds: [embed, embeds] });
   },
 };
