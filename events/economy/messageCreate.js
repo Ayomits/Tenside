ade7972fe3061425a544b0d910f0344cd1bb7e25
Nikita <dimavlaskin9@gmail.com>
2023-12-06 16:19:55 +0300

    ивент для экономики

--- events/economy/messageCreate.js
+++ events/economy/messageCreate.js
@@ -1,6 +1,6 @@
 
 const { Events, Message } = require("discord.js");
-
+const {userModel} = require('../../models/users')
 
 /**
  * @param {Message} message
@@ -9,7 +9,17 @@ const { Events, Message } = require("discord.js");
 module.exports = {
   name: Events.MessageCreate,
   once: false,
+
+  /**
+   * 
+   * @param {Message} message 
+   */
+
   async execute(message) {
-    
+    if (!message.author.bot) {
+      if (!message.content.startsWith(process.env.PREFIX)){
+        await userModel.updateOne({user_id: message.author.id, guild_id: message.guild.id}, {$inc: {balance: 0.5}})
+      }
+    }
   }
 }
