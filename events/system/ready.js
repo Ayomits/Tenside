3cc09048c7d489503eda2944d3cd035e67fceb0b
PVA12 <106029090+PVA12@users.noreply.github.com>
2023-12-08 22:00:39 +0200

    Update ready.js

--- events/system/ready.js
+++ events/system/ready.js
@@ -1,27 +1,53 @@
-const { Events, Client, Activity, ActivityType } = require('discord.js');
-
+const { Events, Client, Activity, ActivityType } = require("discord.js");
+const TimelyModel = require(`../../models/timely`);
+const cron = require(`node-cron`);
 module.exports = {
   name: Events.ClientReady,
   once: true,
   /**
-   * 
-   * @param {Client} client 
+   *
+   * @param {Client} client
    */
   async execute(client) {
     // подгрузка команд, компонентов, создание таблиц в СУБД
 
-    require('../../handlers/system/commandHandler').init(client)
-    require('../../handlers/system/componentsHandler').init("components", "кнопки", client)
-    require('../../handlers/system/componentsHandler').init("components", "модалки", client)
-    require('../../handlers/system/componentsHandler').init("components", "селекты", client)
-    require('../../handlers/system/commandRegister').init(client)
-    await require('../../handlers/system/usersHandler').usersHandler(client)
-    
-    client.user.setStatus("dnd")
+    require("../../handlers/system/commandHandler").init(client);
+    require("../../handlers/system/componentsHandler").init(
+      "components",
+      "кнопки",
+      client
+    );
+    require("../../handlers/system/componentsHandler").init(
+      "components",
+      "модалки",
+      client
+    );
+    require("../../handlers/system/componentsHandler").init(
+      "components",
+      "селекты",
+      client
+    );
+    require("../../handlers/system/commandRegister").init(client);
+    await require("../../handlers/system/usersHandler").usersHandler(client);
+
+    client.user.setStatus("dnd");
     client.user.setActivity({
       name: `Приглядываю за вами :3`,
-      type: ActivityType.Custom
-    })
-    
-  }
-}
+      type: ActivityType.Custom,
+    });
+    console.log(`Запущенно!`);
+    cron.schedule(
+      "00 3 * * *",
+      () => {
+        performDailyTask();
+      },
+      { timezone: "Europe/Moscow" }
+    );
+
+    // Функция для выполнения задачи
+    const performDailyTask = () => {
+      console.log("Выполняю задачу каждый день в 03:00 по московскому времени");
+      TimelyModel.TimelyModel.deleteMany({});
+    };
+  },
+};
