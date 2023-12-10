977d6656490018255b64305907d1303dcd4383c9
PVA12 <106029090+PVA12@users.noreply.github.com>
2023-12-10 19:25:29 +0200

    казино

---
+++ models/casino.js
@@ -0,0 +1,20 @@
+const {Schema} = require("mongoose") 
+const mongoose = require("mongoose") 
+
+const casinoSchema = new Schema({
+  guild_id: {
+    type: String,
+    required: true
+  },
+  balance : {
+    type: Number,
+    default: 500000,
+   },
+  
+})
+
+const casinoModel = mongoose.model("casino", casinoSchema)
+
+module.exports = {
+  casinoModel
+}
