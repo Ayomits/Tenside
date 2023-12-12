53a1f909a30aa54651bfb248e155c65b59336037
Nikita <dimavlaskin9@gmail.com>
2023-12-12 17:27:39 +0300

    изменении логики в аватар

--- commands/entartaiment/avatar.js
+++ commands/entartaiment/avatar.js
@@ -1,66 +1,65 @@
 const {
-  CommandInteraction,
-  EmbedBuilder,
-  PermissionFlagsBits,
-} = require("discord.js");
-const { SlashCommandBuilder } = require("@discordjs/builders");
+    CommandInteraction,
+    EmbedBuilder,
+    PermissionFlagsBits,
+  } = require("discord.js");
+  const { SlashCommandBuilder } = require("@discordjs/builders");
+  
+  module.exports = {
+    data: new SlashCommandBuilder()
+      .setName("avatar")
+      .setDescription("Аватарка и баннер пользователя")
+      .addStringOption((option) =>
+        option
+          .setName("type")
+          .setDescription("type of avatar")
+          .addChoices(
+            {
+              name: "display",
+              value: "display",
+            },
+            {
+              name: "profile",
+              value: "profile",
+            }
+          )
+          .setRequired(true)
+      )
+      .addUserOption((option) =>
+        option
+          .setName("target")
+          .setDescription("Нужный пользователь")
+          .setRequired(false)
+      ),
+  
+    /**
+     *
+     * @param {CommandInteraction} interaction
+     *
+     */
+  
+    async execute(interaction) {
+        const color = '#36393F';
+        const targetUser = interaction.options.getUser("target") || interaction.user;
+        const type = interaction.options.get('type').value
+        const guild = interaction.guild;
+        const member = await guild.members.fetch(targetUser.id)
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("avatar")
-    .setDescription("Аватарка и баннер пользователя")
-    .addStringOption((option) =>
-      option
-        .setName("type")
-        .setDescription("type of avatar")
-        .addChoices(
-          {
-            name: "display",
-            value: "display",
-          },
-          {
-            name: "profile",
-            value: "profile",
-          }
-        )
-        .setRequired(true)
-    )
-    .addUserOption((option) =>
-      option
-        .setName("target")
-        .setDescription("Нужный пользователь")
-        .setRequired(false)
-    ),
+        const bannerUrl = (await member.fetch()).user.bannerURL({size: 4096})
+        const avatarUrl = type !== "display" ? targetUser.displayAvatarURL({size: 2048}) :  member.displayAvatarURL({size: 2048})
 
-  /**
-   *
-   * @param {CommandInteraction} interaction
-   *
-   */
+        const avatarEmbed = new EmbedBuilder()
+                            .setAuthor({ iconURL: avatarUrl, name: member.user.username })
+                            .setImage(avatarUrl)
+                            .setTimestamp(Date.now())
+                            .setColor(color)
 
-  async execute(interaction) {
-    const color = '#36393F'
-    const user = interaction.options.getUser("target") || interaction.user
-    const type = interaction.options.get("type").value;
+        const bannerEmbed = new EmbedBuilder().setTimestamp(Date.now()).setColor(color)
 
-    const avatarUrl = user.avatarURL({size: 2048})
-    const bannerUrl = user.bannerURL({ size: 4096 });
+        !bannerUrl
+            ?bannerEmbed.setDescription("У пользователя нет баннера")
+            :bannerEmbed.setImage(bannerUrl).setDescription("В данный момент дискорд не предоставляет возможность получить баннер пользователя на сервере..")
 
-    const avatarEmbed = new EmbedBuilder()
-      .setAuthor({ iconURL: avatarUrl, name: user.username })
-      .setImage(avatarUrl)
-      .setTimestamp(Date.now())
-      .setColor(color)
-
-    const bannerEmbed = new EmbedBuilder().setTimestamp(Date.now()).setColor(color)
-
-    !bannerUrl
-      ? bannerEmbed.setDescription("У юзера нет баннера")
-      : bannerEmbed.setImage(bannerUrl);
-
-    await interaction.reply({
-      embeds: [avatarEmbed, bannerEmbed],
-      ephemeral: true,
-    });
-  },
-};
+        await interaction.reply({embeds: [avatarEmbed, bannerEmbed], ephemeral: true})
+      }  
+}
