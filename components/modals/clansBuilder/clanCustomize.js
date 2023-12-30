82663b0f79ad479c0a36ef95946a376c1c7c7757
Nikita <dimavlaskin9@gmail.com>
2023-12-30 14:11:35 +0300

    изменение: добавлен deferreply и изменения названия войса и канал

--- components/modals/clansBuilder/clanCustomize.js
+++ components/modals/clansBuilder/clanCustomize.js
@@ -11,6 +11,7 @@ module.exports = {
    */
 
   async execute(interaction) {
+    await interaction.deferReply()
     const config = JSON.parse(await fs.promises.readFile(path.resolve('configs', 'store.json')))
     const result = await clanModel.findOne({
       guild_id: interaction.guildId,
@@ -19,11 +20,11 @@ module.exports = {
 
 
     if (result.clanBalance < Number(config.customize)) {
-      return await interaction.reply({content: "невозможно сменить оформление клану", ephemeral: true})
+      return await interaction.followUp({content: "невозможно сменить оформление клану", ephemeral: true})
     }
 
     if (result.clanLevel < 2) {
-      return await interaction.reply({content: "У вас маленький уровень клана!", ephemeral: true})
+      return await interaction.followUp({content: "У вас маленький уровень клана!", ephemeral: true})
     }
 
     const clanName = interaction.fields.getField('clanName').value
@@ -41,13 +42,24 @@ module.exports = {
     })
 
     try{
-      (await interaction.guild.roles.cache.get(result.clanRole).setColor(clanHex)).setName(clanName)
+      console.log("дошел до ролей");
+      interaction.guild.roles.cache.get(result.clanRole).setColor(clanHex).setName(clanName)
+      console.log("прошёл роли");
     }
     catch (err) {
-      await interaction.channel.send({content: "Не удалось сменить hex роль"})
-    }
+      return await interaction.channel.send({content: "Что-то пошло не так" + `\n ${err}`})
+     }
 
-    return await interaction.reply({content: "Все параметры успешно изменены", ephemeral: true})
+    try {
+      console.log("дошел до каналов");
+      interaction.guild.channels.cache.get(result.clanChat).setName(clanName)
+      interaction.guild.channels.cache.get(result.clanVoice).setName(clanName)
+      console.log("прошёл каналы");
+    }catch (err) {
+      return await interaction.channel.send({content: "Что-то пошло не так" + `\n ${err}`})
+     }
+
+    return await interaction.followUp({content: "Все параметры успешно изменены", ephemeral: true})
     
     
   }
